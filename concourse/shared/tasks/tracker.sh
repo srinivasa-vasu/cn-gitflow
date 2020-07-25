876c7a50d458182c25462152cda62bf7345ecd98
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2020-07-26 02:03:24 +0800

    [Finishes #173996946]

--- concourse/shared/tasks/tracker.sh
+++ concourse/shared/tasks/tracker.sh
@@ -3,6 +3,6 @@
 set -xe
 
 cd git-repo
-echo "Commit id $(cat git-repo/.git/short_ref)" >> comment/output.txt
-cat git-repo/.git/commit_message >> comment/output.txt
-echo "Commit by $(cat git-repo/.git/committer)" >> comment/output.txt
+echo "Commit id $(cat git-repo/.git/short_ref)" >> comments/output.txt
+cat git-repo/.git/commit_message >> comments/output.txt
+echo "Commit by $(cat git-repo/.git/committer)" >> comments/output.txt
