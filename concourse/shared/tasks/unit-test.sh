23cf59fd32828f9d70049b3d25097ec5b004867b
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2020-07-28 20:22:52 +0800

    pipeline update

--- concourse/shared/tasks/unit-test.sh
+++ concourse/shared/tasks/unit-test.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-set -xeu
-
-echo $LOCAL_REPO
-echo $REPO_KEY
-
 export LOCAL_REPO=$LOCAL_REPO
 export REPO_KEY=$REPO_KEY
 export REPO_SECRET=$REPO_SECRET
+
+set -xeu
 
 cd git-repo
 gradle test
