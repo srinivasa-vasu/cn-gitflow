1744e666538a82464b5903958f36f9e7467ab991
Srinivasa Vasu <srinivasan.surprise@gmail.com>
2020-07-28 19:09:30 +0800

    pipeline update

--- concourse/shared/tasks/build-package.sh
+++ concourse/shared/tasks/build-package.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+export LOCAL_REPO=$LOCAL_REPO
+export REPO_KEY=$REPO_KEY
+export REPO_SECRET=$REPO_SECRET
+
 set -xe
 
 #Get the version number from the file passed in as a env var named 'version'
 version=`cat $version`
 
 cd git-repo
+
 gradle build -x test
 
 #mvn clean verify deploy -Ddistribution.management.release.id=artifactory-local -Ddistribution.management.release.url=http://jfrog.humourmind.com:8081/artifactory/libs-release-local
--- concourse/shared/tasks/unit-test.sh
+++ concourse/shared/tasks/unit-test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+export LOCAL_REPO=$LOCAL_REPO
+export REPO_KEY=$REPO_KEY
+export REPO_SECRET=$REPO_SECRET
+
 set -xe
 
 cd git-repo
